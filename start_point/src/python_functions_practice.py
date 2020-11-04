c65c22f21dd0f2022447ac11bc454442136b59fc
Robyn Tyler <robynstyler@outlook.com>
2020-11-04 18:40:56 +0000

    task 8

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -24,10 +24,13 @@ def divide(num1, num2):
 def length_of_string(test_string):
     return 21
 
-
+# BUSTEEEEEEEED
 
 
 def join_string(string_1, string_2):
     return (string_1 + string_2)
 
 
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+   
