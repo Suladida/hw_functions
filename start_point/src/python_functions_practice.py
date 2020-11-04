96e90b5893a8c09fd10cd9e4ece863da71212fce
Robyn Tyler <robynstyler@outlook.com>
2020-11-04 17:54:09 +0000

    test 5

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -8,9 +8,12 @@ def add(num1, num2):
     return num1 + num2
 
 
-def subtract(number1, number2):
-    return number1 - number2
+def subtract(num1, num2):
+    return num1 - num2
 
 
 def multiply(num1, num2):
     return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
