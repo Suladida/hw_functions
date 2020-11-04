e9e9c04e3974495bfea6b2614017f253a72361af
Robyn Tyler <robynstyler@outlook.com>
2020-11-04 18:53:49 +0000

    Task 12 - month abbr

--- start_point/src/python_functions_practice.py
+++ start_point/src/python_functions_practice.py
@@ -39,3 +39,9 @@ def number_to_full_month_name(month):
     import calendar
     list(calendar.month_name)   
     return(calendar.month_name[month])
+
+def number_to_short_month_name(month):
+    import calendar
+    list(calendar.month_abbr)
+    return(calendar.month_abbr[month])
+
